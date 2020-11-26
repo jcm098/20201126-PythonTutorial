5c4391b199c5222015e1ef833990650cb59704ba
roomten <jcm098@icloud.com>
2020-11-26 17:56:55 +0000

    Continued tutorial

--- main.py
+++ main.py
@@ -1,3 +1,52 @@
+# https://repl.it/talk/learn/Learn-To-Code-In-Python/7484
+
 print('Hello world!')
 
 # Write some comments!
+
+'''
+Dear PYer,
+I am confused about how you said you could use triple quotes to make
+SUPER
+LONG
+COMMENTS
+!
+
+I am wondering if this is true,
+and if so, 
+I am wondering if this is correct.
+
+Could you help me with this?
+
+Thanks,
+Random guy who used your tutorial.
+'''
+print('Testing')
+
+x = 5
+y = 7
+z = x*y # 35
+print(z) # => 35
+
+x = 5
+x = str(x)
+b = '5'
+b = int(b)
+print('x = ', x, '; b = ', str(b), ';') # => x = 5; b = 5;
+print('x = '+ x+ '; b = '+ str(b)+ ';') # => x = 5; b = 5;
+
+x = 4
+a = x + 1; print(a)
+a = x - 1; print(a)
+a = x * 2; print(a)
+a = x / 2; print(a)
+
+a += 1; print(a)
+a -= 1; print(a)
+a *= 2; print(a)
+a /= 2; print(a)
+
+x = 'hello'; print(x) # Good
+x = "hello"; print(x) # Good
+# x = "hello' # ERRORRR!!!
+
