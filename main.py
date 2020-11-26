22ae432d711ddcd0acafce0c015d4f15c861d7f0
roomten <jcm098@outlook.com>
2020-11-26 21:23:30 +0000

    more tutoring

--- main.py
+++ main.py
@@ -50,3 +50,47 @@ x = 'hello'; print(x) # Good
 x = "hello"; print(x) # Good
 # x = "hello' # ERRORRR!!!
 
+x = 'Hello everybody!'
+y = x[1]; print(y) # 'e'
+y = x[-1]; print(y) # '!'
+y = x[5]; print(y) # ' '
+y = x[1:]; print(y) # 'ello everybody!'
+y = x[:-1]; print(y) # 'Hello everybod'
+y = x[2:-3]; print(y) # 'llo everyb'
+
+x = " Testing, testing, testing, testing       "
+print(x.strip())
+print(len(x))
+print(x.lower())
+print(x.upper())
+print(x.replace('test', 'runn'))
+print(x.split(','))
+
+print('Type something: ')
+x = input()
+print('Here is what you said: ', x)
+
+print('Here is what you said: ', input('Type something: '))
+
+import random
+print(random.randint(3,5)) # Prints a random number between 3 and 5
+
+from time import sleep
+for i in range(100):
+     print('Hello', str(i))
+     sleep(.3)
+
+import time
+for number in range(100):
+     print(number)
+     time.sleep(.1)
+
+#while True: # Runs forever
+#     print('Hello World!')
+
+import random
+position = '<placeholder>'
+while position != 1: # will run at least once
+    position = random.randint(1, 10)
+    print(position)
+
